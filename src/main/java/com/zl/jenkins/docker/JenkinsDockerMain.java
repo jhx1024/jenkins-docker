04dd6d93dcc4e4f740c7d670379245d5e5542057
jhx1024 <jhx13145200@163.com>
2021-03-26 15:29:08 +0800

    commit gitHub

---
+++ src/main/java/com/zl/jenkins/docker/JenkinsDockerMain.java
@@ -0,0 +1,11 @@
+package com.zl.jenkins.docker;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/zl/jenkins/docker/controller/JenkinsDockerController.java
@@ -0,0 +1,13 @@
+package com.zl.jenkins.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsDockerController {
+
+    @GetMapping("/jenkinsDocker")
+    public String test(){
+        return "this is a jenkins  Docker Test";
+    }
+}
